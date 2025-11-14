7f2f4a4ef09fa03f38272e3fd836891f36b8fecb
Weverton inácio <wevertonlrvinacio2@gmail.com>
2025-11-14 18:47:29 -0300

    Primeiro código e organização do projeto com a packpage

---
+++ src/academy/devdojo/maratonajava/introducao/HelloDevDojo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+
+public class HelloDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
