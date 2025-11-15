57bdb0da07039f6aa83b4826d9ba12900594872a
Weverton inácio <wevertonlrvinacio2@gmail.com>
2025-11-15 09:12:37 -0300

    Aprendi comentários, java doc com html  e suas melhores práticas

--- src/academy/devdojo/maratonajava/introducao/HelloDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/HelloDevDojo.java
@@ -1,8 +1,12 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * <i>Comentário em<i/> <b>java doc<b/>
+ */
 
 public class HelloDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
 }
+
