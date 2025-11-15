50b8fc56853fd76b03db7b170f52355306316ce9
Weverton inácio <wevertonlrvinacio2@gmail.com>
2025-11-15 18:01:44 -0300

    Tipos primitivos - declaração e tamanhos em memória

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -10,7 +10,31 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args){
         //int, double, float, char, byte, short, long, boolean.
 
+        //Inteiros | diferença geral - (Tamanho);
         int minhaIdade = 20;
+        long numeroGrande = 1000000;
+
+        //Decimais;
+        double salarioDouble = 3000;
+        float salarioFloat = 2500;
+
+        //Valores menores;
+        byte idadeByte = 16;
+        short idadeShort = 15;
+
+        //True ou false;
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        //Letras e sobre tabelas;
+        char caractere = 'E';
+        char caractereAscii = 87;
+        char caracetereUnicode = '\u0042';
+
         System.out.println("Minha idade é: " + minhaIdade + " anos.");
+        System.out.println(verdadeiro);
+        System.out.println(caractereAscii);
+        System.out.println(caractere);
+
     }
 }
