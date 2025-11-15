da2ed04bd4bf42297e825fd13395339fa16d8536
Weverton inácio <wevertonlrvinacio2@gmail.com>
2025-11-15 14:24:20 -0300

    Ajustando e organizando as classes, e novo estudo em tipos primitvos

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -4,7 +4,7 @@ package academy.devdojo.maratonajava.introducao;
  * <i>Comentário em<i/> <b>java doc<b/>
  */
 
-public class HelloDevDojo {
+public class Aula01OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * camelCase: variáveis, parâmetros, métodos.
+ * PascalCase: classes, interfaces, enums e records.
+ * CONSTANT_CASE: constantes.
+ */
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        //int, double, float, char, byte, short, long, boolean.
+
+        int minhaIdade = 20;
+        System.out.println("Minha idade é: " + minhaIdade + " anos.");
+    }
+}
